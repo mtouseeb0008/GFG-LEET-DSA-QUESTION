5e576306bfa751c346489d28becbd14e806e1dfb
MD TOUSEEB <touseeb1001bbk@gmail.com>
2023-10-27 21:54:23 +0530

    Added solution - LeetHub

---
+++ Minimum Deletions - GFG/minimum-deletions.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int minimumNumberOfDeletions(string S) { 
+        // code here
+        int len = longestPalinSubseq(S);
+        return S.size()-len;
+    } 
+    int longestPalinSubseq(string S) {
+        string s1 = S;
+    reverse(S.begin(), S.end());
+    string s2 = S;
+    int n = S.length();
+    int dp[n + 1][n + 1];
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[n][n];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string S;
+        cin >> S;
+        Solution obj;
+        cout << obj.minimumNumberOfDeletions(S) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
